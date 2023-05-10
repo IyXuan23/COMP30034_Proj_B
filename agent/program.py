ac6e3186ac4bed87139dcfbfc7920dba4ae676ca
HengIyXuan <henyh@student.unimelb.edu.au>
2023-05-10 20:57:01 +1000

    removed sorting

--- agent/program.py
+++ agent/program.py
@@ -341,12 +341,8 @@ def moveHeuristic(boardstate: dict, agent: Agent, currPlayer: PlayerColor) -> Ac
         
     #no cells in danger, we can play aggressively
     #attempt to attack other opp cells that are free
-
-    #sort the board so that we attempt to spread the higher power first
-    sortedBoard = list(boardstate.items())
-    sortedBoard.sort(key=lambda x: x[1][1], reverse=True)
-
-    for cell in sortedBoard:
+    
+    for cell in boardstate.items():
         if cell[1][0] == currPlayer:
             for dir in HexDir:
                 newPos = cell[0]
